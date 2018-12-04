45878d96d2adeeeebece0c33b82d17432effdac3
Braedon Gough <braedongough@Braedons-MacBook-Pro.local>
2018-12-04 19:03:02 +0100

    setup production build and server

--- src/app.js
+++ src/app.js
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense } from './actions/expenses'
-import moment from 'moment'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
-
-store.dispatch(addExpense({ description: 'gas bill', amount: 90000, createdAt: moment().add(1, 'day').valueOf() }))
-store.dispatch(addExpense({ description: 'rent', amount: 12345, createdAt: moment().subtract(1, 'day').valueOf() }))
-store.dispatch(addExpense({ description: 'water bill', amount: 90300, createdAt: moment().valueOf() }))
 
 const jsx = (
     <Provider store={store}>
