245c64df3a26759e3d3f52bf5899300aa2816a0a
Braedon Gough <braedongough@Braedons-MacBook-Pro.local>
2018-12-05 19:23:56 +0100

    add formmating to list item component

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
 
 export const ExpenseListItem = ({ description, amount, createdAt, id }) => (
     <div>
-        <Link to={`/edit/${id}`}> <h3>{description}</h3></Link>
-        <p>amount: {amount}, Created at: {createdAt}</p>
+        <Link to={`/edit/${id}`}>
+            <h3>{description}</h3>
+        </Link>
+        <p>
+            {`Amount:${numeral((amount / 100)).format('$0,0.00')}`},
+            {`Created at: ${moment(createdAt).format('MMMM Do, YYYY')}`}
+        </p>
     </div>
 )
 
